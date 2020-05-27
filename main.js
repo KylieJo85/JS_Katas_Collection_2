eb7aef2cf3dc0453f9476c4e7ca3362cff8c83e2
Kylie Jo McCafferty <kyliemccafferty85@icloud.com>
2020-05-26 20:41:53 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b
+}
+console.log(add(3, 3))
+
+let multiply=3
+
+for ( let index=3; index<=3 ;index++){
+    multiply=  index + index + index;
+}
+console.log ((multiply ))
+
+let power= 3
+
+for ( let index=0; index<=3; index++){
+    power= power + power + power +index
+    console.log ((power))
+}
+
+let factorial= 4
+
+for ( let index =0-1; index >=1; index--){
+factorial= index + index
+console.log ((factorial))
+}
+
