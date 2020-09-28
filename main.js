2d3b1b6fddd3fb8d7d8219431c701d9040d0e3d1
Kylie Jo McCafferty <kyliemccafferty85@icloud.com>
2020-09-28 19:43:44 -0400

    Finally got the factorial function to work properly.

--- main.js
+++ main.js
@@ -26,15 +26,19 @@ function power(a, b) {
     return d
 }
 
-console.log(power(3, 4))
+console.log(power(3, 3))
 
 
 
-function factorial(a, b) {
-    let f = 0
-    for (let index = 0; index < b; index++) {
-        f = power(a, f)
+function factorial (a) {
+    let f= 1
+    for (let index = 1; index <=a; index++ )
+    {
+        f = multiply (f,index)
     }
     return f
 }
-console.log (factorial(4,3))
+console.log(factorial (5))
+
+
+//Colin helped me to figure out the logic behind the factorial function because I was stuck.//
